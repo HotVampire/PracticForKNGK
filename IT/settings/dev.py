38011533a7ebbf6f8146c0f80ac2b5a1d3c193f1
Darkleizing <101994645+Darkleizing@users.noreply.github.com>
2023-04-11 12:56:48 +0300

    WTF/1
    dawfwafawdaw

---
+++ IT/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-suakm%_f*6mmwy1t$%5_9mel#flb6j$oi3o36k5_*9azwm4^e7"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
